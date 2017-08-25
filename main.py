f8d3b0c22510a3132b55f9d5037ba0ac40e421f6
Elwing-Chou <birdfan8814@gmail.com>
2017-08-25 22:02:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,17 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import ssl
+import scrapy
 
-def returnAllChildAnchors(href_list):
-    # 等等我們要回傳的子節點list
-    result_list = []
-    for single_href in href_list:
-        try:
-            html = urlopen(single_href)
-            bsObj = BeautifulSoup(html, "html.parser")
-            for anchor in bsObj.findAll("a"):
-                if anchor.has_attr('href') and anchor['href'].startswith('/wiki/') and not anchor['href'].startswith('/wiki/Wikipedia'):
-                    #print(anchor['href'])
-                    result_list.append('https://zh.wikipedia.org' + anchor['href'])
-                    if anchor.has_attr('title'):
-                        print('title = ', anchor['title'])
-        except:
-            print('open ', single_href, ' error')
-    return result_list
+class DmozSpider(scrapy.Spider):
+    name = "dmoz"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
 
-ssl._create_default_https_context = ssl._create_unverified_context
-startUrl = ['https://zh.wikipedia.org/wiki/%E7%BA%A6%E7%BF%B0%C2%B7%E5%88%97%E4%BE%AC']
-stopUrl = 'https://zh.wikipedia.org/wiki/%E8%BF%88%E5%85%8B%E5%B0%94%C2%B7%E4%B9%94%E4%B8%B9'
-times = 0
-
-while True:
-    print("times", times)
-    startUrl = returnAllChildAnchors(startUrl)
-    print(times, startUrl)
-    times = times + 1
-    if stopUrl in startUrl:
-        print("find in ", times, " times")
-        break
-
-
+    def parse(self, response):
+        for sel in response.xpath('//ul/li'):
+            title = sel.xpath('a/text()').extract()
+            link = sel.xpath('a/@href').extract()
+            desc = sel.xpath('text()').extract()
+            print title, link, desc
 
